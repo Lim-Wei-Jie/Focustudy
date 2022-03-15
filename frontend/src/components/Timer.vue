04abbeb9880a868f4460c300f76176c7d4d123c3
FIRST_NAME LAST_NAME <89060200+sophiaaachow@users.noreply.github.com>
2022-03-15 21:29:33 +0800

    Changed eslint settings, started testing timer component on default about page

---
+++ frontend/src/components/Timer.vue
@@ -0,0 +1,14 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name: "Timer"
+
+    }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Timer></Timer>
   </div>
 </template>
+
+<script>
+import Timer from "@/components/Timer.vue";
+
+export default {
+  name: "AboutView",
+  components: {
+    Timer
+  }
+}
+</script>
