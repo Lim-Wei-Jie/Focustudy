5c4df33df39d3d52a70953c10f57ebe7507887e1
RickyGoh <89062463+RickyGoh@users.noreply.github.com>
2022-03-19 15:47:39 +0800

    rating system UI is up

---
+++ frontend/src/views/RatingView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Rating></Rating>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Rating from "@/components/Rating.vue";
+
+export default {
+  name: "rating",
+  components: {
+    Rating
+  }
+}
+</script>
