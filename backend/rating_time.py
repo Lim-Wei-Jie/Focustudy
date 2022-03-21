36d8a44b7d9789b9b9d5700dae652ceac02d1aff
FIRST_NAME LAST_NAME <89060200+sophiaaachow@users.noreply.github.com>
2022-03-21 23:22:50 +0800

    Update rating_time.py

--- backend/rating_time.py
+++ backend/rating_time.py
@@ -2,73 +2,56 @@
 #Jsonify serializes data to JavaScript Object Notation (JSON) format
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import extract, func
 from flask_cors import CORS
 from os import environ
 
 app = Flask(__name__)
+# set dbURL=mysql+mysqlconnector://root@localhost:3306/rating 
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 CORS(app)
 
-
-db = SQLAlchemy(app)
-
 class Rating(db.Model):
     __tablename__ = 'rating'
  
     ratingId = db.Column(db.Integer, primary_key=True)
     productivity = db.Column(db.Integer, nullable=False)
-
     
-    #constructor
- 
+    # constructor
     def __init__(self,ratingId , productivity):
         #set the properties when created
         self.ratingId = ratingId
         self.productivity = productivity
- 
-        
         
     # enables our object to be represented as a JSON string
- 
     def json(self):
         return {"ratingId": self.ratingId, "productivity": self.productivity}
-    
-    
-@app.route("/posttime", methods=['POST'])
-def create_rate():
-    
-    
-    
+
+# http://127.0.0.1:5000/addRating
+@app.route("/addRating", methods=['POST'])
+def addRating():
     data = request.get_json()
+    rating = Rating(0, **data)
     
- 
     try:
-        db.session.add(data)
+        db.session.add(rating)
         db.session.commit()
     except:
         return jsonify(
             {
                 "code": 500,
-                "data": {
-                    "isbn13": "7170"
-                },
-                "message": "An error occurred posting the rating"
+                "data": "An error occurred posting the rating"
             }
         ), 500
  
     return jsonify(
         {
             "code": 201,
-            "data": "rating posted"
+            "data": "Rating posted"
         }
     ), 201
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-else:
-    app.run(host='0.0.0.0', port=6000, debug=True)
