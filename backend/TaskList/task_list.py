56c179d47d4179eb0e152621ebef862f377fd5cb
huiqingkoh <kohuiqing@gmail.com>
2022-03-24 09:28:58 +0800

    Removed unnecessary module

--- backend/TaskList/task_list.py
+++ backend/TaskList/task_list.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from os import environ
-import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/TaskList'
