8a331a38da0eaa0860453d6fad67dadde66e88da
RickyGoh <89062463+RickyGoh@users.noreply.github.com>
2022-03-28 14:59:16 +0800

    delete uncessary code

--- frontend/src/endpoint/endpoint.js
+++ frontend/src/endpoint/endpoint.js
@@ -12,21 +12,6 @@ export function catchRate(record) {
         });
 };
 
-export function getRate() {
-    let api_endpoint = `http://127.0.0.1:5000/getRating`;
-    axios
-        .get(api_endpoint)
-        .then((response) => {
-            console.log(response.data);
-            //console.log(response.data)
-            //let email = response.data.data.ratings
-            //console.log(email)
-            return 322
-        })
-        .catch((error) => {
-            console.log(error)
-        });
-};
 
 
 
