31a0545aa474bec9a3faf736fcced46536d8122a
Lim-Wei-Jie <weijie.lim.2020@scis.smu.edu.sg>
2022-03-29 18:29:03 +0800

    create connection to broker

--- backend/Error/amqp_setup.py
+++ backend/Error/amqp_setup.py
@@ -1,2 +1,12 @@
 import pika
 from os import environ
+
+hostname = environ.get('rabbit_host') or 'localhost'
+port = environ.get('rabbit_port') or 5672
+
+# connect to the broker and set up a communication channel in the connection
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host=hostname, port=port,
+        heartbeat=3600, blocked_connection_timeout=3600,
+))
