cf23a3893d6a002b28fd4ec430b2715e5851d84a
Lim-Wei-Jie <weijie.lim.2020@scis.smu.edu.sg>
2022-03-29 21:45:57 +0800

    set up analytics complex MS

---
+++ backend/Analytics/analytics.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import os, sys
+from os import environ
+
+import requests
+from invokes import invoke_http
+
+import amqp_setup
+import pika
+import json
+
+app = Flask(__name__)
+CORS(app)
