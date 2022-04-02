548e3cda022987ccc2a01060b9ab93dcf831d535
Lim-Wei-Jie <weijie.lim.2020@scis.smu.edu.sg>
2022-04-02 16:57:50 +0800

    set up activity log MS

---
+++ backend/ActivityLog/activity_log.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA IF NOT EXISTS activity_log;
+USE activity_log;
+
+DROP TABLE IF EXISTS activity_log;
+CREATE TABLE activity_log
+(
+    activityId INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(320) NOT NULL,
+    date VARCHAR(320) NOT NULL,
+    duration INT NOT NULL,
+    tasks NVARCHAR(MAX) NOT NULL
+);
