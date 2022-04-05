7a1f925195b4d39447de405c1d2f56c3b36b26e5
FIRST_NAME LAST_NAME <89060200+sophiaaachow@users.noreply.github.com>
2022-04-05 23:10:54 +0800

    Delete test.sql

--- backend/Timer/test.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO timer (email, startDate, duration) VALUES
-
-/* test email filter */
-("b@gmail.com", "2022-03-25", 3600),
-
-("a@gmail.com", "2022-03-25", 3600),
-("a@gmail.com", "2022-03-25", 3600),
-("a@gmail.com", "2022-03-25", 5400),
