8a0d92c074777b1e39204d1c6f5442470d729447
Lim-Wei-Jie <weijie.lim.2020@scis.smu.edu.sg>
2022-04-06 22:06:41 +0800

    deleted files

--- frontend/src/components/TaskList.vue
+++ frontend/src/components/TaskList.vue
@@ -1,83 +1,120 @@
 <template>
+  <!-- eslint-disable -->
   <div class="container">
-    <Header @show-task="showAdd" :showAddTask="showAddTask" />
-    <AddTask v-if="showAddTask" @add-task="addTask" />
-    <Tasks
-      @delete-task="deleteTask"
-      :tasks="tasks"
-    />
+
+    <div class="title text-center mb-2">
+      Task List
+    </div>
+
+    <!-- task list -->
+    <ul class="my mx-0 p-0">
+      <!-- each row -->
+      <li v-for="task in tasks" :key="task.id" class="d-flex border rounded my-2 py-1 px-3">
+        <!-- each task -->
+        <div class="col-lg-11 col-10 m-0 p-0">
+          <p :class="{ done: task.done }">{{task.task_description}}</p>
+        </div>
+        <!-- icons for done and delete task -->
+        <div class="col-lg-1 col-2 d-flex m-0 p-0 justify-content-between align-items-center">
+          <span @click="toggleDone(task)"><i class="far fa-check-circle text-success me-1"></i></span>
+          <span @click="delCurrTask(task.id)"><i class="far fa-trash-alt text-danger"></i></span>
+        </div>
+      </li>
+    </ul>
+
+    <!-- add task -->
+    <div class="input-group">
+      <input
+        class="form-control form-control-sm shadow-none"
+        type="text"
+        name="newTask"
+        v-model="newTask"
+        @keyup.enter="onEnter"
+        placeholder="Task Name"
+        maxlength="30"
+      />
+      <div class="input-group-append">
+        <button @click.prevent="addNewTask" type="button" class="btn btn-sm btn-outline-dark">Add</button>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
-import Tasks from "@/components/Tasks.vue";
-import AddTask from "@/components/AddTask.vue";
-import { getTasks, deleteTask } from "../endpoint/endpoint.js";
+import { ref } from 'vue';
+import { getTasks, deleteTask, addTask } from "../endpoint/endpoint.js";
+import { mapState } from "vuex"
 
 export default {
-  name: "TaskList",
-  components: {
-    Header,
-    Tasks,
-    AddTask,
-  },
   data() {
     return {
-      tasks: [],
-      showAddTask: false,
-      // render: true,
-    };
+      tasks: ref([]),
+      newTask: ''
+    }
   },
+  setup() {
+    
+  },
+
+  computed: {
+    ...mapState(["email"]),
+  },
+
+  created() {
+    this.allTasks()
+  },
+
   methods: {
-    deleteTask(id) {
-      // console.log("task", id);
+    allTasks() {
+      getTasks({email: this.email})
+      .then((taskData) => {
+        for (var id of Object.keys(taskData.data)) {
+          this.tasks.push({
+            id: id,
+            task_description: taskData.data[id]["task_description"],
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    },
+
+    delCurrTask(id) {
       if (confirm("Are you sure?")) {
         deleteTask({
-          email: this.$store.state.email,
-          task_id: id,
+          email: this.email,
+          task_id: id
         });
-        // this.force();
       }
     },
-    toggleReminder(id) {
-      // console.log("reminder", id);
-      this.tasks = this.tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
-      );
+
+    toggleDone(task) {
+      task.done = !task.done
     },
-    addTask() {
-      // this.tasks = [...this.tasks, task];
-      console.log(this.tasks);
-      this.showAddTask = false;
-      // this.force();
-    },
-    showAdd() {
-      this.showAddTask = !this.showAddTask;
-    },
-    allTasks() {
-      getTasks({
-        email: this.$store.state.email,
+
+    addNewTask() {
+      if (!this.newTask) {
+        alert("Please add a task.")
+        return
+      }
+
+      addTask({
+        email: this.email,
+        task_description: this.newTask,
       })
-        .then((retrievedTasks) => {
-          this.tasks = [];
-          for (var id of Object.keys(retrievedTasks.data)) {
-            this.tasks.push({
-              id: id,
-              task_description: retrievedTasks.data[id]["task_description"],
-            });
-          }
-          console.log(this.tasks);
-        })
-        .catch((error_message) => {
-          console.log(error_message);
-        });
+
+      this.newTask = "";
     },
-  },
-  created() {
-    this.allTasks();
-  },
-};
+
+    onEnter() {
+      this.addNewTask()
+    },
+
+    
+  }
+}
 </script>
 
 <style scoped>
@@ -90,31 +127,14 @@ export default {
   color: black;
 }
 
-/* nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-} */
-
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
 
-/* body {
-  font-family: 'Poppins', sans-serif;
-} */
-
 .container {
-  max-width: 300px;
+  max-width: 400px;
   margin: auto;
   overflow: auto;
   min-height: 300px;
@@ -123,5 +143,14 @@ nav a.router-link-exact-active {
   border-radius: 20px;
   background: #f7f8f7;
   box-shadow: 0.5px 5px 10px 12px #e4e4e4;
+}
+
+.title {
+  font-size: 20px;
+  color: #043631;
+}
+
+.done {
+  text-decoration: line-through;
 }
 </style>
